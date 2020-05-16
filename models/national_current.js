7bc41b64ef2084026025d83a3f2039975e514d14
Unni Ramachandran <unnipbvr@gmail.com>
2020-05-15 21:17:24 -0400

    Lint changes

--- models/national_current.js
+++ models/national_current.js
@@ -1,11 +1,15 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const National_Current = sequelize.define('National_Current', {
-    confirmed: DataTypes.INTEGER,
-    active: DataTypes.INTEGER,
-    recovered: DataTypes.INTEGER
-  }, {});
-  National_Current.associate = function(models) {
+  const National_Current = sequelize.define(
+    "National_Current",
+    {
+      confirmed: DataTypes.INTEGER,
+      active: DataTypes.INTEGER,
+      recovered: DataTypes.INTEGER,
+    },
+    {}
+  );
+  National_Current.associate = function (models) {
     // associations can be defined here
   };
   return National_Current;
--- models/national_history.js
+++ models/national_history.js
@@ -1,12 +1,16 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const National_History = sequelize.define('National_History', {
-    dateReported: DataTypes.DATEONLY,
-    confirmed: DataTypes.INTEGER,
-    active: DataTypes.INTEGER,
-    recovered: DataTypes.INTEGER
-  }, {});
-  National_History.associate = function(models) {
+  const National_History = sequelize.define(
+    "National_History",
+    {
+      dateReported: DataTypes.DATEONLY,
+      confirmed: DataTypes.INTEGER,
+      active: DataTypes.INTEGER,
+      recovered: DataTypes.INTEGER,
+    },
+    {}
+  );
+  National_History.associate = function (models) {
     // associations can be defined here
   };
   return National_History;
