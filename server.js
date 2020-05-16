859163a4f496c69cb72b9edb36c3592c6e1cfdeb
Unni Ramachandran <unnipbvr@gmail.com>
2020-05-15 21:16:31 -0400

    Set up Express Server

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var db = require("./models");
+
+// const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)ß
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+// Routes
+// app.use(routes);
+
+var syncOptions = {};
+syncOptions.force = process.env.SYNC_MODEL === "true" ? true : false;
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelizeConnection.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
+
+module.exports = app;
